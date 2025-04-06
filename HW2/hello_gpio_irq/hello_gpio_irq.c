88ee34a531fdedc673ae557e54be58119a0bac5f
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-05 22:03:24 -0500

    Update hello_gpio_irq.c

--- HW2/hello_gpio_irq/hello_gpio_irq.c
+++ HW2/hello_gpio_irq/hello_gpio_irq.c
@@ -8,6 +8,42 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
+ // Pico W devices use a GPIO on the WIFI chip for the LED,
+ // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
+ #ifdef CYW43_WL_GPIO_LED_PIN
+ #include "pico/cyw43_arch.h"
+ #endif
+ 
+ #ifndef LED_DELAY_MS
+ #define LED_DELAY_MS 250
+ #endif
+ 
+ // Perform initialisation
+ int pico_led_init(void) {
+ #if defined(PICO_DEFAULT_LED_PIN)
+     // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+     // so we can use normal GPIO functionality to turn the led on and off
+     gpio_init(PICO_DEFAULT_LED_PIN);
+     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+     return PICO_OK;
+ #elif defined(CYW43_WL_GPIO_LED_PIN)
+     // For Pico W devices we need to initialise the driver etc
+     return cyw43_arch_init();
+ #endif
+ }
+ 
+ // Turn the led on or off
+ void pico_set_led(bool led_on) {
+ #if defined(PICO_DEFAULT_LED_PIN)
+     // Just set the GPIO on or off
+     gpio_put(PICO_DEFAULT_LED_PIN, led_on);
+ #elif defined(CYW43_WL_GPIO_LED_PIN)
+     // Ask the wifi "driver" to set the GPIO on or off
+     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+ #endif
+ }
+
+
 #define GPIO_WATCH_PIN 2
 
 static char event_str[128];
@@ -27,13 +63,19 @@ int main() {
 
     gpio_init(GPIO_WATCH_PIN);
     gpio_set_irq_enabled_with_callback(GPIO_WATCH_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
 
     // Wait forever
     while (1){
         printf("Hello, world!\n");
         char msg[100];
-        sprintf(msg,"Button pressed %d times.", &counter);
+        sprintf(msg, "Button pressed %d times.", &counter);
         printf(msg);
+        pico_set_led(true);
+        sleep_ms(LED_DELAY_MS);
+        pico_set_led(false);
+        sleep_ms(LED_DELAY_MS);
         sleep_ms(1000);
     };
 }
