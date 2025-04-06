d7e083e332e4539dbd8caf91a719fb9c5c7956a0
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-06 13:01:33 -0500

    Update hello_usb.c

--- HW2/hello_usb/hello_usb.c
+++ HW2/hello_usb/hello_usb.c
@@ -7,10 +7,25 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#ifdef CYW43_WL_GPIO_LED_PIN
+#include "pico/cyw43_arch.h"
+#endif
+
+#ifndef LED_DELAY_MS
+#define LED_DELAY_MS 250
+#endif
+
 int main() {
     stdio_init_all();
+    int rc = pico_led_init();
+    hard_assert(rc == PICO_OK);
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+
+        pico_set_led(true);
+        sleep_ms(LED_DELAY_MS);
+        pico_set_led(false);
+        sleep_ms(LED_DELAY_MS);
     }
 }
