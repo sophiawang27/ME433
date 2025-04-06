a933fe2f3f23908ae34dbdc927d20df693be589b
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-06 13:55:48 -0500

    Update hello_usb.c

--- HW2/hello_usb/hello_usb.c
+++ HW2/hello_usb/hello_usb.c
@@ -114,3 +114,31 @@ int main() {
         sleep_ms(250);
     }
 }
+
+static const char *gpio_irq_str[] = {
+    "LEVEL_LOW",  // 0x1
+    "LEVEL_HIGH", // 0x2
+    "EDGE_FALL",  // 0x4
+    "EDGE_RISE"   // 0x8
+};
+
+void gpio_event_string(char *buf, uint32_t events) {
+for (uint i = 0; i < 4; i++) {
+    uint mask = (1 << i);
+    if (events & mask) {
+        // Copy this event string into the user string
+        const char *event_str = gpio_irq_str[i];
+        while (*event_str != '\0') {
+            *buf++ = *event_str++;
+        }
+        events &= ~mask;
+
+        // If more events add ", "
+        if (events) {
+            *buf++ = ',';
+            *buf++ = ' ';
+        }
+    }
+}
+*buf++ = '\0';
+}
