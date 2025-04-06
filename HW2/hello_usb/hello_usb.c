e7db5b4eaa5d8ffc6790f70f5232e0b33d8cde0b
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-06 13:13:41 -0500

    Update hello_usb.c

--- HW2/hello_usb/hello_usb.c
+++ HW2/hello_usb/hello_usb.c
@@ -15,30 +15,35 @@
 #define LED_DELAY_MS 250
 #endif
 
+#ifndef PICO_LED_GP14
+#define PICO_LED_GP14 14
+#endif
+
 // Perform initialisation
 int pico_led_init(void) {
-    #if defined(PICO_DEFAULT_LED_PIN)
-        // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
-        // so we can use normal GPIO functionality to turn the led on and off
-        gpio_init(PICO_DEFAULT_LED_PIN);
-        gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-        return PICO_OK;
-    #elif defined(CYW43_WL_GPIO_LED_PIN)
-        // For Pico W devices we need to initialise the driver etc
-        return cyw43_arch_init();
-    #endif
-    }
-    
-    // Turn the led on or off
-    void pico_set_led(bool led_on) {
-    #if defined(PICO_DEFAULT_LED_PIN)
-        // Just set the GPIO on or off
-        gpio_put(PICO_DEFAULT_LED_PIN, led_on);
-    #elif defined(CYW43_WL_GPIO_LED_PIN)
-        // Ask the wifi "driver" to set the GPIO on or off
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
-    #endif
-    }
+#if defined(PICO_LED_GP14)
+    // A device like Pico that uses a GPIO for the LED will define PICO_DEFAULT_LED_PIN
+    // so we can use normal GPIO functionality to turn the led on and off
+    gpio_init(PICO_LED_GP14);
+    gpio_set_dir(PICO_LED_GP14, GPIO_OUT);
+    return PICO_OK;
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // For Pico W devices we need to initialise the driver etc
+    return cyw43_arch_init();
+#endif
+}
+
+// Turn the led on or off
+void pico_set_led(bool led_on) {
+#if defined(PICO_LED_GP14)
+    // Just set the GPIO on or off
+    gpio_put(PICO_LED_GP14, led_on);
+#elif defined(CYW43_WL_GPIO_LED_PIN)
+    // Ask the wifi "driver" to set the GPIO on or off
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_on);
+#endif
+}
+
 
 int main() {
     stdio_init_all();
