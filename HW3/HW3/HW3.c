1c719a92f6660aab3f7f49faee84fd8106a27c96
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-07 21:08:15 -0500

    Update HW3.c

--- HW3/HW3/HW3.c
+++ HW3/HW3/HW3.c
@@ -5,9 +5,15 @@
 int main()
 {
     stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
+    printf("Start!\n");
+ 
+    while (1) {
+        char message[100];
+        scanf("%s", message);
+        printf("message: %s\r\n",message);
+        sleep_ms(50);
     }
 }
