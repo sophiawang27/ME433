23c54ef4f5ef7f6f63164b9b47bdd5966bf5ac49
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-10 15:39:55 -0500

    created hw4

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -11,6 +11,19 @@
 #define PIN_SCK  18
 #define PIN_MOSI 19
 
+void writeDAC(int channel, float volt);
+
+static inline void cs_select(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 0);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
+
+static inline void cs_deselect(uint cs_pin) {
+    asm volatile("nop \n nop \n nop"); // FIXME
+    gpio_put(cs_pin, 1);
+    asm volatile("nop \n nop \n nop"); // FIXME
+}
 
 
 int main()
@@ -18,7 +31,7 @@ int main()
     stdio_init_all();
 
     // SPI initialisation. This example will use SPI at 1MHz.
-    spi_init(SPI_PORT, 1000*1000);
+    spi_init(SPI_PORT, 1000);
     gpio_set_function(PIN_MISO, GPIO_FUNC_SPI);
     gpio_set_function(PIN_CS,   GPIO_FUNC_SIO);
     gpio_set_function(PIN_SCK,  GPIO_FUNC_SPI);
@@ -32,5 +45,30 @@ int main()
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+        int i = 0;
+        float t = 0;
+        for(i=0; i<100; i++){
+            t = t+0.1;
+            float v = sin(t); // change this to be correct (need to change freq and amplitude)
+            writeDAC(0,v);
+            writeDAC(1, tri_wave); // triangle wave
+            sleep_ms(10);
+        }
     }
+}
+
+void writeDAC(int channel, float voltage){
+    uint8_t data[2];
+    int len = 2;
+    uint16_t d = 0;
+    d = d | channel <<15;
+    d = d | 0b111 <<< 12;
+    uint16_t v = voltage * / 
+    d = d | v << 2
+
+    data[0] = d >>8;
+    data[1] = d & 0xFF;; // making 512
+    cs_select(PIN_CS);
+    spi_write_blocking(SPI_PORT, data, len); // where data is a uint8_t array with length len
+    cs_deselect(PIN_CS);
 }
