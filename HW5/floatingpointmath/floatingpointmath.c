65c4d8881c14b2bf4f0a3ea4111f4bccb458dd07
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-15 13:47:34 -0500

    Update floatingpointmath.c

--- HW5/floatingpointmath/floatingpointmath.c
+++ HW5/floatingpointmath/floatingpointmath.c
@@ -9,10 +9,18 @@ int main()
     printf("Enter two floats to use:");
     scanf("%f %f", &f1, &f2);
     volatile float f_add, f_sub, f_mult, f_div;
-    f_add = f1+f2;
-    f_sub = f1-f2;
-    f_mult = f1*f2;
-    f_div = f1/f2;
+
+    absolute_time_t t1 = get_absolute_time();
+    for(int i=0;i<1000;i++){
+        f_add = f1+f2;
+    }
+    absolute_time_t t2 = get_absolute_time();
+    uint64_t start_time = to_us_since_boot(t1);
+    uint64_t end_time = to_us_since_boot(t2);
+    //f_sub = f1-f2;
+    //f_mult = f1*f2;
+    //f_div = f1/f2;
+
     printf("\nResults: \n%f+%f=%f \n%f-%f=%f \n%f*%f=%f \n%f/%f=%f\n", f1,f2,f_add, f1,f2,f_sub, f1,f2,f_mult, f1,f2,f_div);
     while (true) {
 
