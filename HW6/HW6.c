37a7efa5ccd5a62d07c778e8f55c95399a004a2c
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-17 21:49:04 -0500

    Update HW6.c

--- HW6/HW6.c
+++ HW6/HW6.c
@@ -24,6 +24,7 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
+    i2c_setup();
     // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     while (true) {
@@ -31,23 +32,34 @@ int main()
         sleep_ms(100);
         gpio_put(25,1);
         sleep_ms(100);
+
     }
 }
 
+// turn on communication w address and gpio pins on extender
 void i2c_setup(void){
-    unsigned char dir = 0b0000001;
-    i2c_write_blocking(i2c_default, address, dir, 1, false);// talk to i2c chip, give address
+    unsigned char dir = 0b01000000;
+    unsigned char buff[2];
+    buff[0] =  0x00; // sfr iodir
+    buff[1] = 0b01111111; // value to turn gp0 input gp7 output
+    i2c_write_blocking(i2c_default, dir, buff, 2, false);// talk to i2c chip
 
 }
 
-
+// write the value of an output on i2c
 void setPin(unsigned char address, unsigned char register, unsigned char value){
-    i2c_write_blocking(i2c_default, address, buf, 2, false);
-    i2c_write_blocking(i2c_default, address, register, 2, false);
-    i2c_write_blocking(i2c_default, address, value, 2, false);
+    unsigned char buff[2];
+    buff[0] = 0x0A; // olat sfr
+    buff[1] = value;
+    i2c_write_blocking(i2c_default, address, buff, 2, false);// talk to i2c chip
     
 }
 
+// read the value of an input on i2c
 unsigned char readPin(unsigned char address, unsigned char register){
+    unsigned char reg = 0x09;
+    unsigned char buf;
+    i2c_write_blocking(i2c_default, address, &reg, 1, true);  // true to keep master control of bus
+    i2c_read_blocking(i2c_default, address, &buf, 1, false);  // false - finished with bus
 
 }
