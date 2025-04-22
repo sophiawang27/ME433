54c8373e6ec25ae2ee64a3f65a7f37da12f0f63b
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-22 12:54:13 -0500

    hi

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -24,7 +24,10 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
+
+    // initialize OLED display
+    ssd1306_setup();
+
 
     while (true) {
         printf("Hello, world!\n");
