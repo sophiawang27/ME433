9f6bc46d2bbfff57e33a9d955e1fc52f386de090
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-22 13:13:51 -0500

    Update HW7.c

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -27,10 +27,39 @@ int main()
 
     // initialize OLED display
     ssd1306_setup();
+    ssd1306_clear();
+    ssd1306_update();
 
 
     while (true) {
-        printf("Hello, world!\n");
+        char message[50];
+        ssd1306_clear(); // clear all bits
+        sprintf(message, "hello");
+        drawMessage(10, 20, message); // keep drawing until you hit the null
+
+
+        ssd1306_update(); // final action
         sleep_ms(1000);
+    }
+}
+
+void drawMessage(int x, int y, char * m){
+    int i = 0;
+    while (m[i] != 0){
+        drawLetter(x + i*5, y, m[i]); // can add spacing between letters
+        i++;
+    }
+
+}
+
+void drawLetter(int x, int y, char c){ // c is ascii letter to draw
+    int j;
+    for (j=0; j<5; j++){
+        char col = ASCII[c-0x20][j]; // skipping non displayable characters
+        int i = 0;
+        for (i = 0; i<8; i++){
+            char bit = (col >> i)& 0b1;
+            ssd1306_drawPixel(x+j, y+i, bit);
+        }
     }
 }
