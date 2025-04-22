100fbf96c33e539c127efbb9c3a0f54cd4160052
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-04-22 14:23:33 -0500

    uh

--- HW7/HW7.c
+++ HW7/HW7.c
@@ -3,15 +3,17 @@
 #include "hardware/i2c.h"
 #include "ssd1306.h"
 #include "font.h"
+#include "display.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 8
-#define I2C_SCL 9
+#define I2C_SDA 16
+#define I2C_SCL 17
 
-
+void drawMessage(int x, int y, char * m);
+void drawLetter(int x, int y, char c);
 
 int main()
 {
@@ -33,15 +35,19 @@ int main()
 
     while (true) {
         char message[50];
-        t = read_time
-        ssd1306_clear(); // clear all bits
-        sprintf(message, "hello");
-        drawMessage(10, 20, message); // keep drawing until you hit the null
+        // get the start time
+        unsigned int t1 = to_us_since_boot(get_absolute_time());  
+
+        // clr and print the message
+        ssd1306_clear();
+        sprintf(message, "i <3 u");
+        drawMessage(0,0, message); // keep drawing until you hit the null
+        unsigned int t2 = to_us_since_boot(get_absolute_time());  
+        unsigned int tdiff = t2 - t1; // process time in microseconds
+        uint64_t fps = (uint64_t)(1/(tdiff*1000000));// turn time to frames per second
+        sprintf(message, "%u", fps);
+        drawMessage(0,20,message);// print to the bottom of the display
         ssd1306_update(); // final action
-        t2 = read_time
-        tdiff = t2 - 1 // in microseconds
-        // turn time to frames per second
-        // print to the bottom of the display
         sleep_ms(1000);
     }
 }
