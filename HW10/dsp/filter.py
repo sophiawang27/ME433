0bed1338ab20c2ef8216cf4f992f4900288dd564
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-05-01 20:07:01 -0500

    Update filter.py

--- HW10/dsp/filter.py
+++ HW10/dsp/filter.py
@@ -28,7 +28,7 @@ def plotdata(t, data1):
     plt.title('Signal vs Time')
     plt.show()
 
-def fft(t, d):
+def fft(t, d, filtered=None):
     dt = 1.0/10000.0 # 10kHz
 # sample rate = number of data points / total time of samples
     Fs = len(d)/t[-1] # sample rate
@@ -43,25 +43,46 @@ def fft(t, d):
     Y = np.fft.fft(y)/n # fft computing and normalization
     Y = Y[range(int(n/2))] # throw away repeated values
 
+    z = filtered # the data to make the fft from
+    n = len(z) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(n/2))] # one side frequency range
+    Z = np.fft.fft(z)/n # fft computing and normalization
+    Z = Z[range(int(n/2))] # throw away repeated values
+
     fig, (ax1, ax2) = plt.subplots(2, 1)
     ax1.plot(t,y,'b')
+    ax1.legend('Unfiltered Data')
+    if (filtered != None):
+        ax1.plot(t, filtered, 'r')
+        ax1.legend('Unfiltered Data', 'Filtered Data')
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Amplitude')
     ax2.loglog(frq,abs(Y),'b') # plotting the fft
+    ax2.legend('Unfiltered Data')
+    if (filtered != None):
+        ax2.plot(frq, abs(Z), 'r')
+        ax2.legend('Unfiltered Data', 'Filtered Data')
     ax2.set_xlabel('Freq (Hz)')
     ax2.set_ylabel('|Y(freq)|')
     plt.show()
     return Fs
 
+# Write a loop that averages the last X number of data points and saves the result in a new list. 
+# The new list will have X fewer data points, or you can assume the X data points before you started were 0. 
 def maf(mafnum, t, d):
     mafA = []
     mafTA = []
     avg = 0
-    for i in range(mafnum, len(d)):
-        avg = (sum(d[i-mafnum:i]))/mafnum
-        mafTA.append(avg)
-        
-    return t[mafnum:], mafTA
+    for i in range(len(d)):
+        if (i<mafnum):
+            mafA.append(0)
+        else:
+            avg = (sum(d[(i-mafnum):i]))/mafnum
+            mafA.append(avg)
+    return t, mafA
 
 
 def iir(t, d):
@@ -78,25 +99,33 @@ def iir(t, d):
 def fir():
     print("hi")# need floating point coefficients
 
-
+# simple plotting of fft
 tA, dataA = opencsv('sigA.csv')
-samp_rate = fft(tA, dataA)
-print("the sampling rate is ", samp_rate)
+# samp_rate = fft(tA, dataA)
+# print("the sampling rate is ", samp_rate)
 tB, dataB = opencsv('sigB.csv')
-samp_rate = fft(tB, dataB)
-print("the sampling rate is ", samp_rate)
+# samp_rate = fft(tB, dataB)
+# print("the sampling rate is ", samp_rate)
 tC, dataC = opencsv('sigC.csv')
-samp_rate = fft(tC, dataC)
-print("the sampling rate is ", samp_rate)
+# samp_rate = fft(tC, dataC)
+# print("the sampling rate is ", samp_rate)
 tD, dataD = opencsv('sigD.csv')
-samp_rate = fft(tD, dataD)
-print("the sampling rate is ", samp_rate)
-#plotdata(tA, dataA)
-# tB, dataB = opencsv('sigB.csv')
-# plotdata(tB, dataB)
-# tC, dataC = opencsv('sigC.csv')
-# plotdata(tC, dataC)
-# tD, dataD = opencsv('sigD.csv')
-# plotdata(tD, dataD)
+# samp_rate = fft(tD, dataD)
+# print("the sampling rate is ", samp_rate)
 
-# t, mafdata = maf(10, tA, dataA)
+# maf
+t, mafdata = maf(100, tA, dataA)
+#fft(tA, dataA, mafdata)
+t, mafdata = maf(50, tB, dataB)
+fft(tB, dataB, mafdata)
+t, mafdata = maf(100, tA, dataC)
+#fft(tC, dataC, mafdata)
+t, mafdata = maf(50, tD, dataD)
+fft(tD, dataD, mafdata)
+
+
+# iir
+
+
+
+#fir
