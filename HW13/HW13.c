c8b74c9b792f52afaed3f32279ab18d2993ff72a
sophiawang27 <sophiawang2027@u.northwestern.edu>
2025-05-13 13:11:37 -0500

    files

--- HW13/HW13.c
+++ HW13/HW13.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "helper.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -8,33 +9,14 @@
 #define I2C_PORT i2c0
 #define I2C_SDA 8
 #define I2C_SCL 9
-// config registers
-#define CONFIG 0x1A
-#define GYRO_CONFIG 0x1B
-#define ACCEL_CONFIG 0x1C
-#define PWR_MGMT_1 0x6B
-#define PWR_MGMT_2 0x6C
-// sensor data registers:
-#define ACCEL_XOUT_H 0x3B
-#define ACCEL_XOUT_L 0x3C
-#define ACCEL_YOUT_H 0x3D
-#define ACCEL_YOUT_L 0x3E
-#define ACCEL_ZOUT_H 0x3F
-#define ACCEL_ZOUT_L 0x40
-#define TEMP_OUT_H   0x41
-#define TEMP_OUT_L   0x42
-#define GYRO_XOUT_H  0x43
-#define GYRO_XOUT_L  0x44
-#define GYRO_YOUT_H  0x45
-#define GYRO_YOUT_L  0x46
-#define GYRO_ZOUT_H  0x47
-#define GYRO_ZOUT_L  0x48
-#define WHO_AM_I     0x75
 
 
 int main()
 {
     stdio_init_all();
+    while (!stdio_usb_connected()) {
+        sleep_ms(100);
+    }
 
     // I2C Initialisation. Using it at 400Khz.
     i2c_init(I2C_PORT, 400*1000);
@@ -50,3 +32,16 @@ int main()
         sleep_ms(1000);
     }
 }
+
+// use i2c_write_blocking and i2c_read_blocking
+// 14 unsigned chars sequentially in one buffer
+// left bitshift by 8 for high
+// OR with the low
+// save in signed 16-bit number
+// multiply by precalculated numbers
+
+// temp 25 C
+
+// draw line on OLED
+// depending on tilt, change line length
+// can separate components
---
+++ HW13/helper.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "helper.h"
